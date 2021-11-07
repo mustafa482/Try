765ba574299e09f0485780f945c1879a5ddb7c7a
unknown <mustafa.mustafayev482@gmail.com>
2021-11-07 16:00:14 +0400

    Update

---
+++ SecondLesson/SecondLesson/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SecondLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte var1 = 45;
+            byte var2;
+            var2 = 36;
+
+            int sum = var1 + var2;
+
+
+            Console.ReadLine();
+        }
+    }
+}
