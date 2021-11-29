7bbbf5da9c211df88858717001e72d717ac9133b
Mustafa.Mustafayev <mustafa.mustafayev482@gmail.com>
2021-11-29 14:28:25 +0400

    Lesson11

---
+++ Lesson11/Lesson11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+
+namespace Lesson11
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            bool isCountinue;
+            const string ApiUrl = "https://catfact.ninja/fact";
+
+            HttpClient client = new HttpClient();
+            Console.WriteLine("Starting to show facts \r\n \r\n");
+
+            do
+            {
+                var result = await client.GetStringAsync(ApiUrl);
+                Console.WriteLine(result);
+
+                Console.WriteLine();
+
+                Console.WriteLine("Do you want to continue? True or False");
+
+                isCountinue = Convert.ToBoolean(Console.ReadLine());
+            }
+            while (isCountinue);
+        }
+    }
+}
