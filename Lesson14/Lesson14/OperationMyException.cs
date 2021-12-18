5384a6c39d6f43fdfb55a4f54542af64b476bdbc
Mustafa.Mustafayev <mustafa.mustafayev482@gmail.com>
2021-12-18 21:35:25 +0400

    Lesson14

---
+++ Lesson14/Lesson14/OperationMyException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson14
+{
+   public class OperationMyException:Exception
+    {
+        public string OperationName { get; set; }
+
+        public OperationMyException(string message) : base(message)
+        {
+
+        }
+        public OperationMyException(string message,Exception innerException) : base(message,innerException)
+        {
+
+        }
+        public OperationMyException(string message, Exception innerException,string operation):this(message, innerException)
+        {
+            OperationName = operation;
+        }
+
+    }
+}
---
+++ Lesson14/Lesson14/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string LogFiles = @"D:\Desktop\MyDB\Exception.txt";
+            Console.Write("Please Numbers: ");
+            int number1, number2, result = 0;
+            try
+            {
+                number1 = int.Parse(Console.ReadLine());
+                number2 = int.Parse(Console.ReadLine());
+                result = number1 / number2;
+                //string str = null;
+                //str.Trim();
+            }
+            catch (DivideByZeroException exc)
+            {
+                Console.WriteLine("Second number should not be 0");
+                File.AppendAllText(LogFiles, DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss:ff") + '\n' + exc.ToString() + "\n \n");
+                throw new OperationMyException("0 a bolmek olmaz",exc,"Divide");
+            }
+            catch (FormatException exc)
+            {
+                Console.WriteLine("Please enter correct number");
+                File.AppendAllText(LogFiles, DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss:ff") + '\n' + exc.ToString() + "\n \n");
+            }
+            catch (Exception exc)
+            {
+
+                Console.WriteLine("Something unexcepted occured");
+                File.AppendAllText(LogFiles, DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss:ff") + '\n' + exc.ToString() + "\n \n");
+                //throw;
+            }
+            finally
+            {
+                Console.WriteLine("Show always");
+            }
+
+            Console.WriteLine(result);
+            Console.WriteLine("Next Statment");
+        }
+    }
+}
